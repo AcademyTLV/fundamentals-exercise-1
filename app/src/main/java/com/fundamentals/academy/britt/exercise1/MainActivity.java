17c978ba03b5f911eb965323c8a5886882806a3c
Britt Barak <britt.barak@gmail.com>
2017-10-30 08:19:15 +0200

    added send email functionality

--- app/src/main/java/com/fundamentals/academy/britt/exercise1/MainActivity.java
+++ app/src/main/java/com/fundamentals/academy/britt/exercise1/MainActivity.java
@@ -1,12 +1,16 @@
 package com.fundamentals.academy.britt.exercise1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String YOUR_EMAIL = null; //TODO: Optionally add your email
     private EditText editText;
 
     @Override
@@ -18,9 +22,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openActivity(View view) {
-        MessageActivity.start(this, editText.getText().toString());
+        String message = editText.getText().toString();
+        MessageActivity.start(this, message);
     }
 
     public void openEmail(View view) {
+        String message = editText.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"))
+                .putExtra(Intent.EXTRA_EMAIL, YOUR_EMAIL)
+                .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                .putExtra(Intent.EXTRA_TEXT, message);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "No email clients were found on the device", Toast.LENGTH_LONG).show();
+        }
     }
 }
